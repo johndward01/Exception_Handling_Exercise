f6c4e9559043a560f585e4268b95ccaedf3613ec
johndward01 <johndward01@gmail.com>
2023-02-23 10:58:45 -0600

    Add project files.

---
+++ Exception_Handling_Exercise/Methods.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exception_Handling_Exercise
+{
+    internal class Methods
+    {
+        public static void ArithmeticExample()
+        {
+            throw new ArithmeticException();
+        }
+
+        public static void ArrayTypeMismatchExample()
+        {
+            throw new ArrayTypeMismatchException();
+        }
+
+        public static void DivideByZeroExample()
+        {
+            throw new DivideByZeroException();
+        }
+
+        public static void IndexOutOfRangeExample()
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        public static void InvalidCastExample()
+        {
+            throw new InvalidCastException();
+        }
+
+        public static void NullReferenceExample()
+        {
+            throw new NullReferenceException();
+        }
+
+        public static void OutOfMemoryExample()
+        {
+            throw new OutOfMemoryException();
+        }
+
+        public static void OverflowExample()
+        {
+            throw new OverflowException();
+        }
+
+        public static void StackOverflowExample()
+        {
+            throw new StackOverflowException();
+        }
+
+        public static void TypeInitializationExample()
+        {
+            throw new TypeInitializationException("FullTypeNameExample", new ArgumentOutOfRangeException());
+        }
+    }
+}
---
+++ Exception_Handling_Exercise/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exception_Handling_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a try/catch block to handle all of the exceptions!!!
+
+
+            // TODO: Handle the ArithmeticException
+            Methods.ArithmeticExample();
+
+            // TODO: Handle the ArrayTypeMismatchException
+            Methods.ArrayTypeMismatchExample();
+
+
+            // TODO: Handle the DivideByZeroException
+            Methods.DivideByZeroExample();
+
+
+            // TODO: Handle the IndexOutOfRangeException
+            Methods.IndexOutOfRangeExample();
+
+
+            // TODO: Handle the InvalidCastException
+            Methods.InvalidCastExample();
+
+
+            // TODO: Handle the NullReferenceException
+            Methods.NullReferenceExample();
+
+
+            // TODO: Handle the OutOfMemoryException
+            Methods.OutOfMemoryExample();
+
+
+            // TODO: Handle the OverflowException
+            Methods.OverflowExample();
+
+
+            // TODO: Handle the StackOverflowException
+            Methods.StackOverflowExample();
+
+
+            // TODO: Handle the TypeInitializationException
+            Methods.TypeInitializationExample();
+
+
+            // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/exceptions
+        }
+    }
+}

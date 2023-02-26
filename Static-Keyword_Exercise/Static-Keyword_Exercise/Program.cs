e39060cb4718c54a3dae14e0f328f21a858c4f48
johndward01 <johndward01@gmail.com>
2023-02-25 19:05:11 -0600

    init

---
+++ Static-Keyword_Exercise/Static-Keyword_Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Static_Keyword_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
